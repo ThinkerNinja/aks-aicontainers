bbdb676f244cc59575df49f8ff4b3f27edc13dae
Konstantinos Passadis | Azure MVP | MCT <passadis@outlook.com>
2024-06-05 01:54:05 +0300

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -52,4 +52,4 @@ def analyze():
     return jsonify({'results': detailed_results})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=False)
